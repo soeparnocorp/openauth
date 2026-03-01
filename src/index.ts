ae1edf3635be202d912062ef78b28043f27cc13e
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-02 06:21:50 +0700

    index.ts

--- src/index.ts
+++ src/index.ts
@@ -63,9 +63,9 @@ export default {
 				primary: "#ff0000",
 				favicon: "https://id-readtalk.pages.dev/vite.svg",
 				logo: {
-					dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
+					dark: "https://id-readtalk.pages.dev/vite.svg",
 					light:
-						"https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
+						"https://id-readtalk.pages.dev/vite.svg",
 				},
 			},
 			success: async (ctx, value) => {
