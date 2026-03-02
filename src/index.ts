c7ae042bb1fe7ff7c34d1dc3e2d4bf33bd14fb11
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-02 10:48:43 +0700

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import { issuer } from "@openauthjs/openauth";
+import { CloudflareStorage } from "@openauthjs/openauth/storage/cloudflare";
+import { PasswordProvider } from "@openauthjs/openauth/provider/password";
+import { PasswordUI } from "@openauthjs/openauth/ui/password";
+import { createSubjects } from "@openauthjs/openauth/subject";
+import { object, string } from "valibot";
+
+const subjects = createSubjects({
+	user: object({
+		id: string(),
+	}),
+});
+
+export default {
+	fetch(request: Request, env: Env, ctx: ExecutionContext) {
+		const url = new URL(request.url);
+		if (url.pathname === "/") {
+			url.searchParams.set("redirect_uri", "https://id-readtalk.pages.dev/auth/callback");
+			url.searchParams.set("client_id", "your-client-id");
+			url.searchParams.set("response_type", "code");
+			url.pathname = "/authorize";
+			return Response.redirect(url.toString());
+		}
+
+		return issuer({
+			storage: CloudflareStorage({
+				namespace: env.AUTH_STORAGE,
+			}),
+			subjects,
+			providers: {
+				password: PasswordProvider(
+					PasswordUI({
+						sendCode: async (email, code) => {
+							console.log(`Sending code ${code} to ${email}`);
+						},
+						copy: {
+							input_code: "Code (check Worker logs)",
+						},
+					}),
+				),
+			},
+			theme: {
+				title: "READTalk - Auth",
+				primary: "#ff0000",
+				favicon: "https://id-readtalk.pages.dev/vite.svg",
+				logo: {
+					dark: "https://id-readtalk.pages.dev/vite.svg",
+					light: "https://id-readtalk.pages.dev/vite.svg",
+				},
+			},
+			success: async (ctx, value) => {
+				return ctx.subject("user", {
+					id: await getOrCreateUser(env, value.email),
+				});
+			},
+		}).fetch(request, env, ctx);
+	},
+} satisfies ExportedHandler<Env>;
+
+async function getOrCreateUser(env: Env, email: string): Promise<string> {
+	const result = await env.AUTH_DB.prepare(
+		`
+		INSERT INTO user (email)
+		VALUES (?)
+		ON CONFLICT (email) DO UPDATE SET email = email
+		RETURNING id;
+		`,
+	)
+		.bind(email)
+		.first<{ id: string }>();
+	if (!result) {
+		throw new Error(`Unable to process user: ${email}`);
+	}
+	console.log(`Found or created user ${result.id} with email ${email}`);
+	return result.id;
+}
