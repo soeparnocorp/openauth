f5516efc1bfb75bd8e74564e505fea9414fabfc9
SOEPARNO ENTERPRISE <soeparno.corp@outlook.com>
2026-03-03 19:00:01 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -59,13 +59,13 @@ export default {
 				),
 			},
 			theme: {
-				title: "OpenAuth",
+				title: "READTalk OpenAuth",
 				primary: "#ff000000",
-				favicon: "https://workers.cloudflare.com//favicon.ico",
+				favicon: "https://id-readtalk.pages.dev/vite.svg",
 				logo: {
-					dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
+					dark: "https://id-readtalk.pages.dev/vite.svg",
 					light:
-						"https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
+						"https://id-readtalk.pages.dev/vite.svg",
 				},
 			},
 			success: async (ctx, value) => {
